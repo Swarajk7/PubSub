d959a295764fabf164f648294c220ba96686aa15
Jibin Parayil Thomas <jibinpt@gmail.com>
2018-02-03 21:05:47 -0600

    Test

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,7 @@ public class Client{
 
     public static void main(String args[]){
         try{
-
+//
             IServer stub=(IServer) Naming.lookup("rmi://localhost:5000/khadhaa");
 //            System.out.println(stub.add(34,4));
             System.out.println(stub.ping());
